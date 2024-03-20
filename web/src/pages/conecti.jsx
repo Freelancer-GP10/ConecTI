69d22a21472c2cce446e41486a1bc709d4be8232
NauanaCoelho0627 <nauana.santos@sptech.school>
2024-03-20 19:13:02 -0300

    Merge maluco

--- web/src/pages/conecti.jsx
+++ web/src/pages/conecti.jsx
@@ -8,6 +8,8 @@ import "../css/img.css";
 
 // Componentes
     import { CardPay, CardService, CardWorkspace, PlaneBasic, PlanePremium, PlaneUltra } from "../componentes/cards";
+    import { Footer } from "../componentes/footer";
+    import { StilizationsHome } from "../componentes/stilization";
 import { Footer } from "../componentes/footer";
 import { CircleHome } from "../componentes/circle";
 
