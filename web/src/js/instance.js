aa94c461bce46e4fe587f306178e051194eef739
thiagopequeno <thiago.castro@sptech.school>
2024-03-21 10:26:00 -0300

    Vm ip

--- web/src/js/instance.js
+++ web/src/js/instance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instace = axios.create({
 
-     baseURL: 'http://3.87.2.45:8080',
+     baseURL: 'http://44.203.124.198:8080',
      timeout:10000,
      headers: {
           "Content-Type":"application/json",
