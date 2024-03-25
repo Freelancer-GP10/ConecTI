7e59679053d9547b614c9994e89aefa076a2e490
NauanaCoelho0627 <nauana.santos@sptech.school>
2024-03-25 19:25:11 -0300

    ajustes a serem apagados

--- web/src/pages/login.jsx
+++ web/src/pages/login.jsx
@@ -5,6 +5,8 @@ import logopreta from '../assets/logo/logo.png';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import instace from '../js/instance';
+import React from 'react';
+
 
 function Login() {
   const handleSubmit = async (e) => {
